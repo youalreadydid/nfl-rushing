21edaf27da1cfbf3a570c49962b2a41b66096e4f
Mateus <mateus.rstrey@gmail.com>
2021-08-03 20:17:08 -0300

    refactor test

--- test/nfl_rushing_web/live/rushing_statistics_live_test.exs
+++ test/nfl_rushing_web/live/rushing_statistics_live_test.exs
@@ -3,9 +3,13 @@ defmodule NflRushingWeb.RushingStatisticsLiveTest do
 
   import Phoenix.LiveViewTest
 
-  test "disconnected and connected render", %{conn: conn} do
-    {:ok, rushing_statistics_live, disconnected_html} = live(conn, "/")
+  test "disconnected render", %{conn: conn} do
+    {:ok, _rushing_statistics_live, disconnected_html} = live(conn, "/")
     assert disconnected_html =~ "NFL Rushing"
+  end
+
+  test "connected render", %{conn: conn} do
+    {:ok, rushing_statistics_live, disconnected_html} = live(conn, "/")
     assert render(rushing_statistics_live) =~ "Player</th><th>Team</th><th>Pos</th>"
   end
 end
