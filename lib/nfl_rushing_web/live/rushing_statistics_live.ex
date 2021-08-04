4bd58248dbb2e0e067843b215e0b16ef402c3c60
Mateus <mateus.rstrey@gmail.com>
2021-08-03 21:39:14 -0300

    fix stub on dev environment

--- lib/nfl_rushing_web/live/rushing_statistics_live.ex
+++ lib/nfl_rushing_web/live/rushing_statistics_live.ex
@@ -12,7 +12,24 @@ defmodule NflRushingWeb.RushingStatisticsLive do
 
   @impl true
   def handle_info(:update, socket) do
-    stat = NflRushing.Fixtures.build(:rushing_statistics)
+    stat = %NflRushing.RushingStatistics{
+      name: "Shane Vereen",
+      team: "NYG",
+      position: "RB",
+      attemps: 33,
+      attemps_per_game_average: 6.6,
+      total_yards: 158,
+      average_yards_per_attempt: 4.8,
+      yards_per_game: 31.6,
+      total_touchdowns: 1,
+      longest_rush: "25",
+      first_downs: 8,
+      first_down_percentage: 24.2,
+      yards_each_20_plus: 1,
+      yards_each_40_plus: 0,
+      fumbles: 1
+    }
+
     {:noreply, assign(socket, rushing_statistics: [stat])}
   end
 end
