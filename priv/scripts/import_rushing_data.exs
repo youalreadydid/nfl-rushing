5bf02f8988cf1b1be83d23ade6c9dc502d755cd9
Mateus <mateus.rstrey@gmail.com>
2021-08-04 18:30:49 -0300

    fix import script

--- priv/scripts/import_rushing_data.exs
+++ priv/scripts/import_rushing_data.exs
@@ -19,15 +19,8 @@ defmodule ImportRushingStatisticsData do
   end
 
   defp insert_rushing_statistics(stat) do
-    {total_yards, _} = stat["Yds"] |> to_string() |> Integer.parse()
-
-    {longest_rush, longest_rush_touchdown} =
-      if is_integer(stat["Lng"]) do
-        {stat["Lng"], false}
-      else
-        {longest_rush, touchdown} = Integer.parse(stat["Lng"])
-        {longest_rush, touchdown == "T"}
-      end
+    total_yards = parse_total_yards(stat)
+    {longest_rush, longest_rush_touchdown} = parse_longest_rush(stat)
 
     attrs = %{
       name: stat["Player"],
@@ -48,9 +41,27 @@ defmodule ImportRushingStatisticsData do
       fumbles: stat["FUM"]
     }
 
+    fields = Map.keys(attrs)
+
     %RushingStatistics{}
-    |> Ecto.Changeset.cast(attrs, @fields)
+    |> Ecto.Changeset.cast(attrs, fields)
     |> Repo.insert!()
+  end
+
+  defp parse_total_yards(stat) do
+    {total_yards, ""} =
+      stat["Yds"] |> to_string() |> String.replace(~r/[^\d|\.]/, "") |> Integer.parse()
+
+    total_yards
+  end
+
+  defp parse_longest_rush(stat) do
+    if is_integer(stat["Lng"]) do
+      {stat["Lng"], false}
+    else
+      {longest_rush, touchdown} = Integer.parse(stat["Lng"])
+      {longest_rush, touchdown == "T"}
+    end
   end
 end
 
