d5333817869e54967830323261fcbde31c4d2a30
Mateus <mateus.rstrey@gmail.com>
2021-08-04 18:47:27 -0300

    sort statistics by some fields

--- lib/nfl_rushing.ex
+++ lib/nfl_rushing.ex
@@ -4,12 +4,33 @@ defmodule NflRushing do
 
   alias NflRushing.{Repo, RushingStatistics}
 
-  @spec search_rushing_statistics(String.t()) :: list(RushingStatistics.t())
-  def search_rushing_statistics(player_name \\ "%") do
+  @spec search_rushing_statistics(String.t(), keyword()) :: list(RushingStatistics.t())
+  def search_rushing_statistics(player_name, opts \\ []) do
     search_name = "%#{player_name}%"
+    sort = Keyword.get(opts, :sort)
+    order = Keyword.get(opts, :order)
 
     RushingStatistics
     |> where([s], like(s.name, ^search_name))
+    |> order_rushing_statistics_by(sort, order)
     |> Repo.all()
+  end
+
+  defp order_rushing_statistics_by(query, field, order) do
+    fields = ~w(total_yards longest_rush total_touchdowns)a
+    orders = ~w(asc desc)a
+
+    order_fragment =
+      if field == :longest_rush do
+        [{order, :longest_rush}, {order, :longest_rush_touchdown}]
+      else
+        [{order, field}]
+      end
+
+    if field in fields and order in orders do
+      order_by(query, [s], ^order_fragment)
+    else
+      query
+    end
   end
 end
--- lib/nfl_rushing_web/live/rushing_statistics_live.ex
+++ lib/nfl_rushing_web/live/rushing_statistics_live.ex
@@ -5,19 +5,36 @@ defmodule NflRushingWeb.RushingStatisticsLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, rushing_statistics: [], search: nil)}
+    {:ok, assign(socket, rushing_statistics: [], search: nil, sort: nil, order: nil)}
   end
 
   @impl true
   def handle_params(params, _uri, socket) do
-    search = params["search"]
-    stats = NflRushing.search_rushing_statistics(search)
-    {:noreply, assign(socket, rushing_statistics: stats, search: search)}
+    {search, sort, order} = parse_params(params)
+    stats = NflRushing.search_rushing_statistics(search, sort: sort, order: order)
+
+    {:noreply,
+     assign(socket, rushing_statistics: stats, search: search, sort: sort, order: order)}
+  end
+
+  @sort_fields ~w(total_yards longest_rush total_touchdowns)
+  defp parse_params(params) do
+    search = params["search"] || ""
+    sort = if params["sort"] in @sort_fields, do: String.to_atom(params["sort"])
+    order = if params["order"] == "desc", do: :desc, else: :asc
+    {search, sort, order}
   end
 
   @impl true
-  def handle_event("search", %{"q" => name}, socket) do
+  def handle_event("search", %{"q" => name}, %{assigns: assigns} = socket) do
     {:noreply,
-     push_redirect(socket, to: Routes.rushing_statistics_path(socket, :index, search: name))}
+     push_redirect(socket,
+       to:
+         Routes.rushing_statistics_path(socket, :index,
+           search: name,
+           sort: assigns.sort,
+           order: assigns.order
+         )
+     )}
   end
 end
