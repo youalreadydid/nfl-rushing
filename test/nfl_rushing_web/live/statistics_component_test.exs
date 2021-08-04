ef4e6a16e278dc462357105fe438f77bd5a66c7d
Mateus <mateus.rstrey@gmail.com>
2021-08-03 23:43:13 -0300

    format

--- test/nfl_rushing_web/live/statistics_component_test.exs
+++ test/nfl_rushing_web/live/statistics_component_test.exs
@@ -18,23 +18,23 @@ defmodule NflRushingWeb.StatisticsComponentTest do
 
     for stat <- stats do
       assert rendered_component =~
-      """
-          <td>#{stat.name}</td>
-          <td>#{stat.team}</td>
-          <td>#{stat.position}</td>
-          <td>#{stat.attemps}</td>
-          <td>#{stat.attemps_per_game_average}</td>
-          <td>#{stat.total_yards}</td>
-          <td>#{stat.average_yards_per_attempt}</td>
-          <td>#{stat.yards_per_game}</td>
-          <td>#{stat.total_touchdowns}</td>
-          <td>#{stat.longest_rush}</td>
-          <td>#{stat.first_downs}</td>
-          <td>#{stat.first_down_percentage}</td>
-          <td>#{stat.yards_each_20_plus}</td>
-          <td>#{stat.yards_each_40_plus}</td>
-          <td>#{stat.fumbles}</td>
-      """
+               """
+                   <td>#{stat.name}</td>
+                   <td>#{stat.team}</td>
+                   <td>#{stat.position}</td>
+                   <td>#{stat.attemps}</td>
+                   <td>#{stat.attemps_per_game_average}</td>
+                   <td>#{stat.total_yards}</td>
+                   <td>#{stat.average_yards_per_attempt}</td>
+                   <td>#{stat.yards_per_game}</td>
+                   <td>#{stat.total_touchdowns}</td>
+                   <td>#{stat.longest_rush}</td>
+                   <td>#{stat.first_downs}</td>
+                   <td>#{stat.first_down_percentage}</td>
+                   <td>#{stat.yards_each_20_plus}</td>
+                   <td>#{stat.yards_each_40_plus}</td>
+                   <td>#{stat.fumbles}</td>
+               """
     end
   end
 end
