f8e4ce58a308a132f8003ebdabcb9f741d686179
Mateus <mateus.rstrey@gmail.com>
2021-08-18 14:32:41 -0300

    add mock happy path case

--- test/nfl_rushing_web/live/rushing_team_statistics_live_test.exs
+++ test/nfl_rushing_web/live/rushing_team_statistics_live_test.exs
@@ -12,4 +12,13 @@ defmodule NflRushingWeb.RushingTeamStatisticsLiveTest do
     {:ok, live, _disconnected} = live(conn, "/team")
     assert render(live) =~ "</th></tr></table>"
   end
+
+  test "renders all rushing statistics for a team", %{conn: conn} do
+    _stat_one = insert!(:rushing_statistics, %{name: "John Stone", team: "NYG", total_yards: 100, longest_rush: 10, longest_rush_touchdown: true})
+    _stat_two = insert!(:rushing_statistics, %{name: "Max Smith", team: "NYG", total_yards: 50, longest_rush: 4, longest_rush_touchdown: false})
+    {:ok, live, _disconnected} = live(conn, "/team")
+
+    assert render(live) =~ "<th>Team</th><th>Total Yds</th><th>Lng</th>"
+    assert render(live) =~ "<td>NYG</td><td>150</td><td>10T</td>"
+  end
 end
